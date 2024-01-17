9e28a8057957eb9c6047346a92da743be64ed858
Joespk <supakritjoe@gmail.com>
2024-01-18 01:05:54 +0700

    add filterByPromotion

---
+++ app/wwe/page.tsx
@@ -0,0 +1,29 @@
+import { BlogPostDTO, IAllowPromotion } from "@/src/type/dto";
+import { fetchBlogs } from "../actions/fetchblog";
+import Blog from "@/src/components/Blog";
+
+export default async function wwe() {
+  const blogs = await fetchBlogs();
+  const filterByPromotion = (
+    posts: BlogPostDTO[],
+    promotion: IAllowPromotion
+  ): BlogPostDTO[] => {
+    return posts.filter((post) => post.attributes.promotion === promotion);
+  };
+  const wwePosts: BlogPostDTO[] = filterByPromotion(blogs, "wwe");
+  const sortedBlogs = wwePosts.sort(
+    (a: BlogPostDTO, b: BlogPostDTO) => b.id - a.id
+  );
+
+  return (
+    <div className="container mx-auto flex items-center flex-col ">
+      <div className="flex item-center flex-wrap justify-center ">
+        {sortedBlogs?.map((blog: BlogPostDTO) => (
+          <div className="flex-row mx-4 justify-around my-5">
+            <Blog key={blog.id} blog={blog} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
